e1d875cd52ace712d1a4c7ec0d38e9609876412d
dikikhan <99858229+dikikhan@users.noreply.github.com>
2022-03-22 14:23:50 +0700

    Add files via upload

---
+++ HalloWord.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hallo.word;
+
+/**
+ *
+ * @author user
+ */
+public class HalloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hallo Word");
+    }
+    
+}
